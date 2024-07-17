73a5d335472031df61b7c7a35410e27d72c1a55f
subham206 <subhamnayak206@gmail.com>
2024-07-17 16:15:25 +0530

    Created a react-native project

--- firstproject/App.js
+++ firstproject/App.js
@@ -1,39 +1,203 @@
-import React, { useState } from 'react';
-import GettingStarted from './components/GettingStarted';
-import Signup from './components/Signup';
-import Login from './components/Login';
-import Welcome from './components/Welcome';
-import Product from './components/Product';
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { useState } from "react";
 
 export default function App() {
-  const [currentScreen, setCurrentScreen] = useState('GettingStarted');
-  const [userData, setUserData] = useState({username:'',email:'',password:''});
+  const [showPage, setShowPage] = useState("getting-started");
+  const [name, setName] = useState(" ");
+  const [email, setEmail] = useState(" ");
+  const [password, setPassword] = useState(" ");
+  const [loginEmail, setloginEmail] = useState(" ");
+  const [loginPassword, setloginPassword] = useState(" ");
 
-  
-  const renderScreen = () => {
-    switch (currentScreen) {
-      case 'GettingStarted':
-        return <GettingStarted onStart={() => setCurrentScreen('Signup')} />;
-      case 'Signup':
-        return (<Signup 
-          onSignup={(username,email,password) => { 
-          setUserData({username,email,password}); 
-          setCurrentScreen('Login'); }} />);
-      case 'Login':
-        return (<Login 
-          username={userData.username}
-          password={userData.password}
-         onLogin={(username,password) => setCurrentScreen('Welcome')} />);
-      case 'Welcome':
-        return <Welcome username={userData.username} onProductPage={() => setCurrentScreen('Product')} onLogout={() => setCurrentScreen('GettingStarted')} />;
-      case 'Product':
-        return <Product username={userData.username} />;
-      default:
-        return <GettingStarted onStart={() => setCurrentScreen('Signup')} />;
-    }
+  //
+
+  const signup = () => {
+    console.log("Button is being pressed!!");
+    setShowPage("signup");
   };
 
-  return (
-    <>{renderScreen()}</>
-  );
+  const handleLogin = () => {
+    console.log("Button is being pressed!!");
+    setShowPage("login");
+  };
+  const dashboard = () => {
+    console.log("Button is being pressed!!");
+    setShowPage("dashboard");
+  };
+  const logout = () => {
+    console.log("Button is being pressed!!");
+    setShowPage("getting-started");
+  };
+
+  // GETTING STARTED PAGE
+  if (showPage === "getting-started") {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "stretch",
+          backgroundColor: "#ADD8E6",
+        }}
+      >
+        <Text style={{ marginLeft: 50, fontSize: 20, fontWeight: "bold" }}>
+                  HELLO USER
+        </Text>
+
+        <Button title="SIGN UP" color="green" onPress={signup} />
+      </View>
+    );
+  }
+  //SIGN UP PAGE
+  else if (showPage === "signup") {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "stretch",
+          backgroundColor: "#ADD8E6",
+        }}
+      >
+        <Text
+          style={{
+            marginLeft: 120,
+            fontSize: 30,
+            fontWeight: "bold",
+            marginTop: 20,
+            marginBottom: 90,
+          }}
+        >
+          SIGN UP
+        </Text>
+
+        <TextInput
+          style={{
+            marginLeft: 70,
+            height: 60,
+            margin: 12,
+            borderWidth: 1,
+            padding: 10,
+            fontSize: 20,
+          }}
+          placeholder="Enter your Name:"
+          onChangeText={setName}
+        />
+
+        <TextInput
+          style={{
+            marginLeft: 70,
+            height: 60,
+            margin: 12,
+            borderWidth: 1,
+            padding: 10,
+            fontSize: 20,
+          }}
+          placeholder="Enter your email:"
+          onChangeText={setEmail}
+        />
+        <TextInput
+          style={{
+            marginLeft: 70,
+            height: 60,
+            margin: 12,
+            borderWidth: 1,
+            padding: 10,
+            marginBottom: 20,
+            fontSize: 20,
+          }}
+          placeholder="Enter your password:"
+          secureTextEntry={true}
+          onChangeText={setPassword}
+        />
+
+        <TextInput
+          style={{
+            marginLeft: 70,
+            height: 60,
+            margin: 12,
+            borderWidth: 1,
+            padding: 10,
+            fontSize: 20,
+          }}
+          placeholder="confirm password:"
+          secureTextEntry={true}
+        />
+        <Button title="Complete SIGN UP" color="green" onPress={handleLogin} />
+      </View>
+    );
+  }
+  //LOGIN PAGE
+  else if (showPage === "login") {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "stretch",
+          backgroundColor: "#ADD8E6",
+        }}
+      >
+        <Text
+          style={{
+            marginLeft: 120,
+            fontSize: 30,
+            fontWeight: "bold",
+            marginTop: 20,
+            marginBottom: 90,
+          }}
+        >
+          This is our LOGIN page
+        </Text>
+
+        <TextInput
+          style={{
+            marginLeft: 70,
+            height: 60,
+            margin: 12,
+            borderWidth: 1,
+            padding: 10,
+            fontSize: 20,
+          }}
+          placeholder="Enter your email:"
+          onChangeText={setloginEmail}
+        />
+        <TextInput
+          style={{
+            marginLeft: 70,
+            height: 60,
+            margin: 12,
+            borderWidth: 1,
+            padding: 10,
+            marginBottom: 20,
+            fontSize: 20,
+          }}
+          placeholder="Enter your password:"
+          secureTextEntry={true}
+          onChangeText={setloginPassword}
+        />
+
+        <Button title="LOGIN" color="green" onPress={dashboard} />
+      </View>
+    );
+  }
+  //DASHBOARD PAGE
+  else if (showPage === "dashboard") {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "stretch",
+          backgroundColor: "#ADD8E6",
+        }}
+      >
+        <Text style={{ marginLeft: 50, fontSize: 20, fontWeight: "bold" }}>
+          THIS IS THE FINAL RESULT
+        </Text>
+
+        <Button title="LOGOUT" color="red" onPress={logout} />
+      </View>
+    );
+  }
 }
